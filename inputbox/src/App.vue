57b0d8551762b9b98b83624591f2d955b9c659a3
Terminal123 <138257699+Dark-Ai-py@users.noreply.github.com>
2024-09-01 08:11:26 -0500

    template for further work

--- inputbox/src/App.vue
+++ inputbox/src/App.vue
@@ -1,23 +1,6 @@
 <script setup></script>
 
 <template>
-  <div>
-    <label class="form-control">
-      <div class="label">
-        <span class="label-text">Vim inputbox</span>
-      </div>
-      <textarea
-        class="textarea textarea-bordered h-24 textarea-primary"
-        placeholder="Bio"
-      ></textarea>
-      <div class="label">
-        <div class="badge">default</div>
-        <div class="badge badge-neutral">neutral</div>
-        <div class="badge badge-primary">primary</div>
-        <div class="badge badge-secondary">secondary</div>
-        <div class="badge badge-accent">accent</div>
-        <div class="badge badge-ghost">ghost</div>
-      </div>
-    </label>
-  </div>
+  <textarea class="textarea textarea-ghost" placeholder="Bio"></textarea>
+  <div class="badge badge-primary">Normal</div>
 </template>
